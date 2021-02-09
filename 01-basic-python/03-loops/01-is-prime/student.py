d4949e71a9346d623d0506b9c2bff09100994d51
randmon <ccmarques98@gmail.com>
2021-02-09 17:39:36 +0100

    Loops

--- 01-basic-python/03-loops/01-is-prime/student.py
+++ 01-basic-python/03-loops/01-is-prime/student.py
@@ -1,1 +1,6 @@
-# Write your code here
+def is_prime(n):
+    for i in range(2, n): #We gaan delen door elke getal tussen 2 en de nummer zelf
+        if (n % i) == 0: #Niet een priemgetal
+            return False
+    #als er geen 'break' in de 'for' gevonden wordt - dan is het wel een priemgetal
+    return n > 1
