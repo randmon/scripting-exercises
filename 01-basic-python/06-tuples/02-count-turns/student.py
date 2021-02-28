f5ad2fccac0503beaa33cb730bdd6cb479320c7d
randmon <ccmarques98@gmail.com>
2021-02-28 14:38:20 +0100

    tuples

--- 01-basic-python/06-tuples/02-count-turns/student.py
+++ 01-basic-python/06-tuples/02-count-turns/student.py
@@ -1,1 +1,6 @@
-# Write your code here
+def count_turns(ns):
+    count = 0
+    for x, y, z in zip(ns, ns[1:], ns[2:]):
+        if x < y > z or x > y < z:
+            count += 1
+    return count
