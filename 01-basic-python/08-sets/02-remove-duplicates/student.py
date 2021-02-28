cb5d8c2690e43d2c5fceeffe89bb3ec65ee1805e
randmon <ccmarques98@gmail.com>
2021-02-28 16:17:30 +0100

    sets

--- 01-basic-python/08-sets/02-remove-duplicates/student.py
+++ 01-basic-python/08-sets/02-remove-duplicates/student.py
@@ -1,1 +1,8 @@
-# Write your code here
+def remove_duplicates(xs):
+    result = []
+    found = set()
+    for x in xs:
+        if x not in found:
+            found.add(x)
+            result.append(x)
+    return result
