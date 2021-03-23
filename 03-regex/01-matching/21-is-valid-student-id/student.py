0b513713dde735865aac1bb490eb2bd76c8e92eb
randmon <ccmarques98@gmail.com>
2021-03-23 12:33:59 +0100

    idk

--- 03-regex/01-matching/21-is-valid-student-id/student.py
+++ 03-regex/01-matching/21-is-valid-student-id/student.py
@@ -1,1 +1,4 @@
-# Write your code here
+import re
+
+def is_valid_student_id(string):
+    return re.fullmatch('[RrSs]\d{7}', string)
--- 03-regex/01-matching/22-twice-repeated/student.py
+++ 03-regex/01-matching/22-twice-repeated/student.py
@@ -1,1 +1,4 @@
-# Write your code here
+import re
+
+def twice_repeated(string):
+    return re.fullmatch('(.)/1', string)
