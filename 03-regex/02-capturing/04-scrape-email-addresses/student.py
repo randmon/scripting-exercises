b04ca971d8523e1cc48ee19f25b604f00c1fa934
randmon <ccmarques98@gmail.com>
2021-06-03 18:12:46 +0200

    regex sub

--- 03-regex/02-capturing/04-scrape-email-addresses/student.py
+++ 03-regex/02-capturing/04-scrape-email-addresses/student.py
@@ -1,4 +1,4 @@
 import re
 
 def scrape_email_addresses(string):
-    return re.findall(r'([a-zA-Z\d]*@[a-zA-Z\d.]*)', string)
+    return re.findall(r'([a-zA-Z\d.]*@[a-zA-Z\d.]*)', string)
--- 03-regex/03-substituting/02-remove-repeated-words/student.py
+++ 03-regex/03-substituting/02-remove-repeated-words/student.py
@@ -1,1 +1,6 @@
-# Write your code here
+import re
+#Removes repeated words that come after eachother, separated by a space
+#Does not remove all the occurences of one word if they are separated by other different words
+#Example: in ['a', 'b', 'a'] the 2nd 'a' is not removed
+def remove_repeated_words(string):
+    return re.sub(r'([a-zA-Z]+)( \1)+', r'\1', string)
